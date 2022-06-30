8c65d40d04b09ade302a5fde35322920dea29799
guillaume <reygner0224@gmail.com>
2022-06-30 14:54:28 +0200

    add new validates for backend model user and model country

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User < ApplicationRecord
   has_many :join_table_favorite_cities, dependent: :destroy
   has_many :cities, through: :join_table_favorite_cities
 
+  validates :email, presence: true, format: { with: /[a-zA-Z0-9.+-]+@[a-zA-Z]+.+[a-zA-Z]{2,3}/,
+    message: "Please respect email format : email@email.com" }
+  
+  validates :password, presence: true, length: { in: 6..30 }
+
   after_create :welcome_send
 
   def welcome_send
